748051eb9ee1fb3ab106dd892728743023ec53bc
JackLaptop <sandersjack141@gmail.com>
2024-04-28 01:25:34 +0200

    Added forward and back prop for linear function

--- Projects/Mnist/include/NeuralNetwork.h
+++ Projects/Mnist/include/NeuralNetwork.h
@@ -3,15 +3,13 @@
 
 typedef struct 
 {
-    float W1[10 * 784];
-    float B1[10 * 1];
-    float W2[10 * 10];
-    float B2[10 * 1];
-} Parameters;
+    float W[10 * 784];
+    float B[10 * 1];
+} ParametersLinear;
 
 
 void NeuralNetwork(float *h_data, int *h_numImages, int *h_numRows, int *h_numCols);
 
-void ForwardProp(float *d_A, Parameters *d_params, int *d_numImages, int *d_numRows, int *d_numCols, int *h_numImages, int *h_numRows, int *h_numCols);
+void ForwardProp(float *d_A, ParametersLinear *d_params, int *d_numImages, int *d_numRows, int *d_numCols, int *h_numImages, int *h_numRows, int *h_numCols);
 
 #endif
--- Projects/Mnist/src/NeuralNetwork.cu
+++ Projects/Mnist/src/NeuralNetwork.cu
@@ -6,9 +6,9 @@ extern "C" {
     #include "NeuralNetwork.h"
 }
 
-__global__ void linearForwardProp(float* A, float* Z, Parameters *params, int *num_images, int *num_rows, int *num_cols){
-    int idx = blockIdx.y * blockDim.y + threadIdx.y;
-    int idy = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void linearForwardProp(float* A, float* Z, ParametersLinear *params, int *num_images, int *num_rows, int *num_cols){
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int idy = blockIdx.y * blockDim.y + threadIdx.y;
 
     int A_x_dim = 60000;
     int A_y_dim = 784;
@@ -23,14 +23,55 @@ __global__ void linearForwardProp(float* A, float* Z, Parameters *params, int *n
 
     if(idx < Z_x_dim && idy < Z_y_dim){
         for(int t=0; t< W_x_dim; t++){
-            Z_value += A[idx*A_y_dim + idy] * params->W1[t * W_y_dim + idy] + params->B1[t];
+            Z_value += A[idx*A_y_dim + idy] * params->W[t * W_y_dim + idy] + params->B[t];
         }
         
         Z[idx * Z_y_dim + idy] = Z_value;
     }
 }
 
-void ForwardProp(float *d_A, Parameters *d_params, int *d_numImages, int *d_numRows, int *d_numCols, int *h_numImages, int *h_numRows, int *h_numCols){
+__global__ void linearUpdateWeight(float* A, float* dZ, ParametersLinear *params){
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int idy = blockIdx.y * blockDim.y + threadIdx.y;
+
+    int A_x_dim = 60000;
+    int A_y_dim = 784;
+
+    int W_x_dim = 10;
+    int W_y_dim = A_y_dim;
+
+    int dZ_x_dim = 10;
+    int dZ_y_dim = A_y_dim;
+
+    float dW_value = 0.0f;
+
+    float learning_rate = 0.01;
+
+    if(idx < W_x_dim && idy < W_y_dim){
+        for(int i=0; i<dZ_x_dim; i++){
+            dW_value += dZ[idx*W_y_dim + i] * A[idy * A_y_dim + i];
+        }
+
+        params->W[idx * W_y_dim + idy] = params->W[idx * W_y_dim + idy] - learning_rate * dW_value/A_x_dim;
+    }
+}
+
+__global__ void linearUpdateBias(float *dZ, ParametersLinear *params){
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    int dZ_x_dim = 10;
+    int dZ_y_dim = 60000;
+
+    float learning_rate = 0.01;
+
+    if(idx < dZ_x_dim * dZ_y_dim){
+        int dZ_x = idx / dZ_x_dim;
+        int dZ_y = idx % dZ_x_dim;
+        atomicAdd(&params->B[dZ_y], -learning_rate * (dZ[dZ_x * dZ_y_dim + dZ_y] / dZ_y_dim));
+    }
+}
+
+void ForwardProp(float *d_A, ParametersLinear *d_params, int *d_numImages, int *d_numRows, int *d_numCols, int *h_numImages, int *h_numRows, int *h_numCols){
     float *d_Z;
     // Z (A.x, W.y)
     cudaMalloc((void**)&d_Z, *h_numImages * *h_numRows * *h_numCols * sizeof(float));
@@ -52,13 +93,15 @@ void ForwardProp(float *d_A, Parameters *d_params, int *d_numImages, int *d_numR
     
 }
 
+void BackProp(){}
+
 void NeuralNetwork(float *h_data, int *h_numImages, int *h_numRows, int *h_numCols){
 
-    Parameters* h_params = (Parameters*)malloc(sizeof(Parameters));
+    ParametersLinear* h_params = (ParametersLinear*)malloc(sizeof(ParametersLinear));
 
     float *d_data;
     int *d_numImages, *d_numRows, *d_numCols;
-    Parameters *d_params;
+    ParametersLinear *d_params;
 
     cudaMalloc((void**)&d_data, *h_numImages* *h_numRows * *h_numCols * sizeof(float));
 
@@ -66,7 +109,7 @@ void NeuralNetwork(float *h_data, int *h_numImages, int *h_numRows, int *h_numCo
     cudaMalloc((void**)&d_numRows, sizeof(int));
     cudaMalloc((void**)&d_numCols, sizeof(int));
 
-    cudaMalloc((void**)&d_params, sizeof(Parameters));
+    cudaMalloc((void**)&d_params, sizeof(ParametersLinear));
 
     cudaMemcpy(d_data, h_data, *h_numImages* *h_numRows * *h_numCols * sizeof(float), cudaMemcpyHostToDevice);
     
@@ -74,7 +117,7 @@ void NeuralNetwork(float *h_data, int *h_numImages, int *h_numRows, int *h_numCo
     cudaMemcpy(d_numRows, h_numRows, sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_numCols, h_numCols, sizeof(int), cudaMemcpyHostToDevice);
 
-    cudaMemcpy(d_params, h_params, sizeof(Parameters), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_params, h_params, sizeof(ParametersLinear), cudaMemcpyHostToDevice);
 
     // Testing with one forward prop
     ForwardProp(d_data, d_params, d_numImages, d_numRows, d_numCols, h_numImages, h_numRows, h_numCols);
