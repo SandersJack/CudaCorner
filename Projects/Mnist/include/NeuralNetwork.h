3faff7165051a5406ab9aaa06e4b5f91da5f0b32
JackLaptop <sandersjack141@gmail.com>
2024-04-28 17:32:16 +0200

    Fixed issues with dynamic struct and memory allocation. Now need to batch the inputs to reduce the load on the gpu as atm the threads per block are > 1000

--- Projects/Mnist/include/NeuralNetwork.h
+++ Projects/Mnist/include/NeuralNetwork.h
@@ -3,13 +3,15 @@
 
 typedef struct 
 {
-    float W[10 * 784];
-    float B[10 * 1];
+    float *W;
+    float *B;
 } ParametersLinear;
 
 
 void NeuralNetwork(float *h_data, int *h_numImages, int *h_numRows, int *h_numCols);
 
-void ForwardProp(float *d_A, ParametersLinear *d_params, int *d_numImages, int *d_numRows, int *d_numCols, int *h_numImages, int *h_numRows, int *h_numCols);
+void ForwardProp(float *d_X, ParametersLinear *d_params1, ParametersLinear *d_params2, int *d_numImages, int *d_numRows, 
+    int *d_numCols, int *h_numImages, int *h_numRows, int *h_numCols,
+    float *d_Z1, float *d_A1, float *d_Z2, float *d_A2);
 
 #endif
--- Projects/Mnist/src/NeuralNetwork.cu
+++ Projects/Mnist/src/NeuralNetwork.cu
@@ -92,9 +92,14 @@ __global__ void linearForwardProp(float* A, float* Z, ParametersLinear *params, 
 
     float Z_value = 0;
 
+    float *W = params->W;
+    float *B = params->B; 
+
+    //float val = params->W[0];
+
     if(idx < Z_x_dim && idy < Z_y_dim){
-        for(int t=0; t< W_x_dim; t++){
-            Z_value += A[idx*A_y_dim + idy] * params->W[t * W_y_dim + idy] + params->B[t];
+        for(int t=0; t< W_y_dim; t++){
+            Z_value += A[idx*A_y_dim + t] * params->W[t * W_y_dim + idx] + params->B[idy];
         }
         
         Z[idx * Z_y_dim + idy] = Z_value;
@@ -165,37 +170,94 @@ __global__ void linearUpdateBias(float *dZ, ParametersLinear *params){
     }
 }
 
-void ForwardProp(float *d_A, ParametersLinear *d_params, int *d_numImages, int *d_numRows, int *d_numCols, int *h_numImages, int *h_numRows, int *h_numCols){
-    float *d_Z;
-    // Z (A.x, W.y)
-    cudaMalloc((void**)&d_Z, *h_numImages * 10 * sizeof(float));
+void ForwardProp(float *d_X, ParametersLinear *d_params1, ParametersLinear *d_params2, int *d_numImages, int *d_numRows, 
+    int *d_numCols, int *h_numImages, int *h_numRows, int *h_numCols,
+    float *d_Z1, float *d_A1, float *d_Z2, float *d_A2){
 
+    int matrixSize = *h_numImages * 784;
+    printf("Start Forward \n");
     cudaError_t cudaError;
 
     dim3 block_size(8,8);
     dim3 num_of_blocks((*h_numImages+block_size.x-1)/block_size.x,(*h_numRows * *h_numCols+block_size.y-1)/block_size.y);
 
-    linearForwardProp<<<num_of_blocks, block_size>>>(d_A, d_Z, d_params, d_numImages, d_numRows, d_numCols);
+    int singleDimblockSize = 1024;
+    int singleDimnumBlocks = (matrixSize + singleDimblockSize - 1) / singleDimblockSize;
+    printf("Matrix Size %i \n", matrixSize);
+    printf("Num block x threads %i \n", singleDimblockSize * singleDimnumBlocks);
+    printf("Num Threads %i \n", singleDimnumBlocks);
+    /// First Linear Layer
+    linearForwardProp<<<num_of_blocks, block_size>>>(d_X, d_Z1, d_params1, d_numImages, d_numRows, d_numCols);
+    cudaDeviceSynchronize();
+    printf("Lin1 Done \n");
+    cudaError = cudaGetLastError();
+    if (cudaError != cudaSuccess) {
+        printf("Kernel launch error (linearForwardProp1): %s\n", cudaGetErrorString(cudaError));
+        // Handle error appropriately
+        exit(0);
+    }
+    /// Relu Layer
+    reLUForward<<<singleDimblockSize, singleDimnumBlocks>>>(d_Z1, d_A1);
     cudaDeviceSynchronize();
 
     cudaError = cudaGetLastError();
     if (cudaError != cudaSuccess) {
-        printf("Kernel launch error (linearForwardProp): %s\n", cudaGetErrorString(cudaError));
+        printf("Kernel launch error (reLUForward): %s\n", cudaGetErrorString(cudaError));
         // Handle error appropriately
         exit(0);
     }
-    
+    /// Second Linear Layer
+    linearForwardProp<<<num_of_blocks, block_size>>>(d_A1, d_Z2, d_params2, d_numImages, d_numRows, d_numCols);
+    cudaDeviceSynchronize();
+
+    cudaError = cudaGetLastError();
+    if (cudaError != cudaSuccess) {
+        printf("Kernel launch error (linearForwardProp2): %s\n", cudaGetErrorString(cudaError));
+        // Handle error appropriately
+        exit(0);
+    }
+
+    float *d_sum_exp; 
+    cudaMalloc((void**)&d_sum_exp, sizeof(float));
+
+    /// Sum the exponetial 
+    sumExp<<<singleDimblockSize, singleDimnumBlocks>>>(d_Z2, d_sum_exp);
+    cudaDeviceSynchronize();
+
+    cudaError = cudaGetLastError();
+    if (cudaError != cudaSuccess) {
+        printf("Kernel launch error (softMax): %s\n", cudaGetErrorString(cudaError));
+        // Handle error appropriately
+        exit(0);
+    }
+    /// Conduct the SoftMax
+    softMax<<<singleDimblockSize, singleDimnumBlocks>>>(d_Z2, d_A2, d_sum_exp);
+    cudaDeviceSynchronize();
+
+    cudaError = cudaGetLastError();
+    if (cudaError != cudaSuccess) {
+        printf("Kernel launch error (softMax): %s\n", cudaGetErrorString(cudaError));
+        // Handle error appropriately
+        exit(0);
+    }
+
 }
 
 void BackProp(){}
 
 void NeuralNetwork(float *h_data, int *h_numImages, int *h_numRows, int *h_numCols){
 
-    ParametersLinear* h_params = (ParametersLinear*)malloc(sizeof(ParametersLinear));
+    printf("Init Params \n");
+    ParametersLinear* h_params1 = (ParametersLinear*)malloc(sizeof(ParametersLinear));
+    ParametersLinear* h_params2 = (ParametersLinear*)malloc(sizeof(ParametersLinear));
+
+    //h_params1->W = (float*)malloc(10 * 784 * sizeof(float));
+    //h_params1->B = (float*)malloc(10 * 1 * sizeof(float));
+
 
     float *d_data;
     int *d_numImages, *d_numRows, *d_numCols;
-    ParametersLinear *d_params;
+    ParametersLinear *d_params1, *d_params2;
 
     cudaMalloc((void**)&d_data, *h_numImages* *h_numRows * *h_numCols * sizeof(float));
 
@@ -203,16 +265,46 @@ void NeuralNetwork(float *h_data, int *h_numImages, int *h_numRows, int *h_numCo
     cudaMalloc((void**)&d_numRows, sizeof(int));
     cudaMalloc((void**)&d_numCols, sizeof(int));
 
-    cudaMalloc((void**)&d_params, sizeof(ParametersLinear));
+    /// Parameters Memory
+    cudaMalloc((void**)&d_params1, sizeof(ParametersLinear));
+    //cudaMalloc((void**)&d_params2, sizeof(ParametersLinear));
+  
+    float* d_W1;
+    cudaMalloc((void**)&d_W1, 10 * 784 * sizeof(float));
+    cudaMemcpy(&(d_params1->W), &d_W1, sizeof(float*), cudaMemcpyHostToDevice);
 
+    float* d_B1;
+    cudaMalloc((void**)&d_B1, 10 * 1 * sizeof(float));
+    cudaMemcpy(&(d_params1->B), &d_B1, sizeof(float*), cudaMemcpyHostToDevice);
+    
+    /*
+    float* d_W2;
+    cudaMalloc((void**)&d_W1, 10 * 10 * sizeof(float));
+    cudaMemcpy(&(d_params2->W), &d_W2, sizeof(float*), cudaMemcpyHostToDevice);
+    float* d_B2;
+    cudaMalloc((void**)&d_B2, 10 * 1 * sizeof(float));
+    cudaMemcpy(&(d_params2->B), &d_B2, sizeof(float*), cudaMemcpyHostToDevice);
+    */
+    ///
+    
     cudaMemcpy(d_data, h_data, *h_numImages* *h_numRows * *h_numCols * sizeof(float), cudaMemcpyHostToDevice);
     
     cudaMemcpy(d_numImages, h_numImages, sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_numRows, h_numRows, sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_numCols, h_numCols, sizeof(int), cudaMemcpyHostToDevice);
-
-    cudaMemcpy(d_params, h_params, sizeof(ParametersLinear), cudaMemcpyHostToDevice);
+    
+    
+    /// Init the outputs of the forward steps
+    float *d_Z1; // Z1 (A.x, W.y)
+    cudaMalloc((void**)&d_Z1, *h_numImages * 10 * sizeof(float));
+    float *d_A1; // A1 (A.x, W.y)
+    cudaMalloc((void**)&d_A1, *h_numImages * 10 * sizeof(float));
+    float *d_Z2; // Z2
+    cudaMalloc((void**)&d_Z2, *h_numImages * 10 * sizeof(float));
+    float *d_A2; // A2
+    cudaMalloc((void**)&d_A2, *h_numImages * 10 * sizeof(float));
 
     // Testing with one forward prop
-    ForwardProp(d_data, d_params, d_numImages, d_numRows, d_numCols, h_numImages, h_numRows, h_numCols);
+    ForwardProp(d_data, d_params1, d_params2, d_numImages, d_numRows, d_numCols, h_numImages, h_numRows, h_numCols,
+                d_Z1, d_A1, d_Z2, d_A2);
 }
